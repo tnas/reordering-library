a04da3ef8eca0344d688a78b9709b5be0bb31731
tnas <nascimenthiago@gmail.com>
2015-11-30 23:02:58 -0200

    prefix sum working!

--- Reorderings/rcm_unordered.c
+++ Reorderings/rcm_unordered.c
@@ -78,18 +78,20 @@ int pow_uint(int base, const int exp)
 void prefix_sum(const int* counts, int** sums, const int max_level)
 {
 	int level, local_level, chunk_size, index_processors, id_proc, offset_level, 
-		coef_target_proc, target_proc;
+		coef_target_proc, target_proc, count_thread;
 	status_prefix_sum status_ps[NUM_THREADS];
 	
 	*sums = calloc(max_level, sizeof(int));
 	
 // 	max_level = 15;
-// 	*sums = calloc(15, sizeof(int));
-// 	
+// 	*sums = calloc(max_level, sizeof(int));
 // 	int tcounts[15] = { 9, 8, 3, 2, 7, 1, 6, 4, 5, 5, 8, 3, 4, 9, 1}; 
 	
 	chunk_size = iseven(max_level) ? max_level/NUM_THREADS : max_level/NUM_THREADS + 1;
 	offset_level = -chunk_size;
+	count_thread = 0;
+	
+// 	printf("chunk_size: %d\n", chunk_size);
 	
 	#pragma omp parallel private (level, local_level, id_proc, coef_target_proc, target_proc)
 	{
@@ -98,29 +100,29 @@ void prefix_sum(const int* counts, int** sums, const int max_level)
 		
 		#pragma omp critical
 		{
-			#pragma omp flush (offset_level)
+			#pragma omp flush (offset_level, count_thread)
 			offset_level += chunk_size;
 			level = offset_level;
+			id_proc = count_thread++;
 		}
-	
 	
 // 		(*sums)[level] = tcounts[level];
 		(*sums)[level] = counts[level];
-		id_proc = level / NUM_THREADS;
-			
-// 			printf("Thread %d with id_proc: %d\n", omp_get_thread_num(), id_proc);
+		
+		
+// 		printf("Thread %d with id_proc: %d and level: %d\n", omp_get_thread_num(), id_proc, level);
 		
 		for (local_level = level+1; local_level < level+chunk_size && local_level < max_level; ++local_level)
 			(*sums)[local_level] = (*sums)[local_level-1] + counts[local_level];
 		
-		
-// 			#pragma omp single 
-// 			{
-// 			printf("Sums vector after Step 1: ");
-// 			for (count_thread = 0; count_thread < 15; ++count_thread) 
-// 				printf("%d ", (*sums)[count_thread]);
-// 			printf("\n");
-// 			}
+// 		#pragma omp barrier
+// 		#pragma omp single 
+// 		{
+// 		printf("Sums vector after Step 1: ");
+// 		for (local_level = 0; local_level < max_level; ++local_level) 
+// 			printf("%d ", (*sums)[local_level]);
+// 		printf("\n");
+// 		}
 		
 		status_ps[id_proc].initial_prefix_sum 
 			= status_ps[id_proc].curr_prefix_sum 
@@ -129,11 +131,10 @@ void prefix_sum(const int* counts, int** sums, const int max_level)
 			= status_ps[id_proc].last_total_sum
 			= (*sums)[local_level-1];
 		
-// 			printf("Init step 2 => id_proc: %d, initial_prefix_sum: %d, final_prefix_sum: %d, total_sum: %d\n",
-// 				id_proc,
-// 				status_ps[id_proc].initial_prefix_sum,
-// 				status_ps[id_proc].final_prefix_sum, 
-// 				status_ps[id_proc].total_sum);
+// 		printf("Init step 2 => id_proc: %d, curr_prefix_sum: %d, curr_total_sum: %d\n",
+// 			id_proc,
+// 			status_ps[id_proc].curr_prefix_sum,
+// 			status_ps[id_proc].curr_total_sum);
 		
 		#pragma omp barrier
 		
@@ -142,8 +143,8 @@ void prefix_sum(const int* counts, int** sums, const int max_level)
 			target_proc = id_proc ^ pow_uint(2, coef_target_proc);
 			if (target_proc >= NUM_THREADS || target_proc == id_proc) continue;
 			
-// 				printf("id_proc %d sending to target_proc %d because coef_target_proc: %d and pow_uint is %d\n", id_proc, target_proc, coef_target_proc, pow_uint(2, coef_target_proc));
-			printf("id_proc %d sending to target_proc %d\n", id_proc, target_proc);
+// 			printf("id_proc %d sending to target_proc %d because coef_target_proc: %d and pow_uint is %d\n", id_proc, target_proc, coef_target_proc, pow_uint(2, coef_target_proc));
+// 			printf("id_proc %d sending to target_proc %d\n", id_proc, target_proc);
 			
 
 			if (id_proc < target_proc) {
@@ -162,8 +163,8 @@ void prefix_sum(const int* counts, int** sums, const int max_level)
 			
 			#pragma omp barrier
 			
-			#pragma omp single
-			printf("**** finish coef_target_proc %d\n", coef_target_proc);
+// 			#pragma omp single
+// 			printf("**** finish coef_target_proc %d\n", coef_target_proc);
 		}
 		
 // 		printf("Final step => id_proc: %d, initial_prefix_sum: %d, last_prefix_sum: %d, last_total_sum: %d\n",
@@ -193,7 +194,7 @@ void prefix_sum(const int* counts, int** sums, const int max_level)
 // 			level = offset_level;
 // 		}
 		
-		level = id_proc * NUM_THREADS;
+		level = id_proc * chunk_size;
 		
 		for (local_level = level; local_level < level+chunk_size && local_level < max_level; ++local_level)
 			(*sums)[local_level] += status_ps[id_proc].last_prefix_sum;
@@ -221,20 +222,26 @@ void REORDERING_RCM_parallel(MAT* A, int** perm)
 	
 	max_level = count_nodes_by_level(levels, n_nodes, &counts);
 	
-	tcounts = calloc(max_level+1, sizeof(int));
+	tcounts = calloc(++max_level, sizeof(int));
 	tcounts[0] = 0;
-	#pragma omp for
-	for (index = 1; index < max_level+1; ++index)
-		tcounts[index] = counts[index];
 	
-	prefix_sum(tcounts, &sums, max_level+1);
+	#pragma omp parallel for private (index)
+	for (index = 1; index < max_level; ++index)
+		tcounts[index] = counts[index-1];
+	
+// 	printf("Vector tcounts: ");
+// 	for (index = 0; index < max_level+1; ++index)
+// 		printf("%d ", tcounts[index]);
+// 	printf("\n");
+	
+	prefix_sum(tcounts, &sums, max_level);
 	
 	/* Reverse order */
 // 	for (count_nodes = 0; count_nodes < n_nodes; ++count_nodes) 
 // 		(*perm)[n_nodes-1-count_nodes] = tperm[count_nodes]; 
 	
 	printf("Sums vector: ");
-	for (count_nodes = 0; count_nodes < max_level+1; ++count_nodes) 
+	for (count_nodes = 0; count_nodes < max_level; ++count_nodes) 
 		printf("%d ", sums[count_nodes]);
 	printf("\n");
 // 	
