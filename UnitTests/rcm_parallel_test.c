12e45042cfa4101e9267d454ea011b158bd87f76
tnas <nascimenthiago@gmail.com>
2016-01-20 09:20:10 -0200

    suite of test

--- UnitTests/rcm_parallel_test.c
+++ UnitTests/rcm_parallel_test.c
@@ -45,3 +45,182 @@ void test_prefix_sum_parallel_serial(int* counts, int max_level)
 	free(s_sums);
 	free(p_sums);
 }
+
+
+test_result run_test_serial_rcm(const char* path_matrix_file)
+{
+	int root, e;
+	long int bandwidth, envelope, bandwidth_after, envelope_after;
+	int* permutation;
+	double time;
+	MAT* matrix;
+	FILE* matrix_file;
+	test_result result;
+	
+	if ((matrix_file = fopen(path_matrix_file, "r")) == NULL) 
+		exit(1);
+	
+	matrix = (MAT*) malloc (sizeof(MAT));
+	MATRIX_readCSR (matrix, matrix_file);
+	fclose(matrix_file);
+	
+	GRAPH_LS_peripheral(matrix, &root, &e);
+	
+	bandwidth_after = MATRIX_bandwidth(matrix);
+	envelope_after  = MATRIX_envelope(matrix);
+	
+	time = get_time(); 
+	REORDERING_RCM_opt(matrix, &permutation, root);
+	time = (get_time() - time)/100.0;
+	result.time = time;
+	
+	MATRIX_permutation(matrix, permutation);
+	bandwidth = MATRIX_bandwidth(matrix);
+	envelope  = MATRIX_envelope(matrix);	
+	result.bandwidth = bandwidth;
+	
+	free(permutation);
+	MATRIX_clean(matrix);
+	
+	printf("Serial RCM: Band/Env [ %ld / %ld => %ld / %ld ] Time [ %.6f ]\n",
+		bandwidth_after, envelope_after, bandwidth, envelope, time); fflush(stdout);
+	
+	return result;
+}
+
+
+
+test_result run_test_unordered_rcm(const char* path_matrix_file, const int num_threads, const float bfs_chunk_size)
+{
+	int root, e;
+	long int bandwidth, envelope;
+	int* permutation;
+	double time;
+	MAT* matrix;
+	FILE* matrix_file;
+	test_result result;
+	
+	if ((matrix_file = fopen(path_matrix_file, "r")) == NULL) 
+		exit(1);
+	
+	matrix = (MAT*) malloc (sizeof(MAT));
+	MATRIX_readCSR (matrix, matrix_file);
+	fclose(matrix_file);
+	
+	GRAPH_LS_peripheral(matrix, &root, &e);
+	
+	omp_set_num_threads(num_threads);
+	
+	time = get_time(); 
+	Unordered_RCM(matrix, &permutation, root, bfs_chunk_size);
+	time = (get_time() - time)/100.0;
+	result.time = time;
+	
+	MATRIX_permutation(matrix, permutation); 
+	
+	bandwidth = MATRIX_bandwidth(matrix);
+	envelope  = MATRIX_envelope(matrix);	
+	result.bandwidth = bandwidth;
+	
+	free(permutation);
+	MATRIX_clean(matrix);
+	
+	printf("Unordered RCM (Threads %d, Chunk %f): Band/Env [ %ld / %ld ] Time [ %.6f ]\n",
+		num_threads, bfs_chunk_size, bandwidth, envelope, time); fflush(stdout);
+	
+	return result;
+}
+
+
+void run_test_serial_parallel_rcm(const char* path_matrix_file, const int num_threads, const float bfs_chunk_size)
+{
+	run_test_serial_rcm(path_matrix_file);
+	run_test_unordered_rcm(path_matrix_file, num_threads, bfs_chunk_size);
+}
+
+
+void print_head_table_result(FILE* out_file)
+{
+	int count_exec;
+	
+	fprintf(out_file, "|Reordering  | Threads |");
+	for (count_exec = 1; count_exec <= TEST_EXEC_TIMES; ++count_exec)
+		fprintf(out_file, " Exec %d  |", count_exec);
+	fprintf(out_file, "\n");
+	
+	for (count_exec = 1; count_exec <= TEST_EXEC_TIMES + 2; ++count_exec)
+		fprintf(out_file, "---------|");
+	fprintf(out_file, "\n");
+}
+
+
+void run_all_tests()
+{
+	int count_matrix, count_percents, count_exec, count_nthreads;
+	FILE* out_file;
+	test_result tresult;
+	
+	int num_matrices = 7;
+	char* matrices[] = {
+		"../Big-Matrices/inline_1.mtx",
+		"../Big-Matrices/audikw_1.mtx",
+		"../Big-Matrices/dielFilterV3real.mtx",
+		"../Big-Matrices/G3_circuit.mtx",
+		"../Big-Matrices/hugetric-00020.mtx",
+		"../Big-Matrices/delaunay_n24.mtx"
+		"../Big-Matrices/road_usa.mtx"
+	};
+// 	char* matrices[] = {
+// 		"../Matrices/rail_5177.mtx"
+// 	};
+	
+	int num_bfs_percents = 2;
+	float bfs_chunk_percent[] = { .5, .8 };
+	
+	int num_nthreads = 6;
+	int nthreads[] = { 4, 8, 16, 32, 64, 128 };
+	
+	
+	if ((out_file = fopen("run_tests_output.txt", "w")) == NULL) 
+		exit(1);
+
+	for (count_matrix = 0; count_matrix < num_matrices; ++count_matrix)
+	{
+		fprintf(out_file, "-----------------------------------------------------------------------\n");
+		fprintf(out_file, "Tests Execution - Matrix: %s\n", matrices[count_matrix]);
+		fprintf(out_file, "-----------------------------------------------------------------------\n");
+		
+		print_head_table_result(out_file);
+		
+		fprintf(out_file, "|Serial      |    1    |");
+		for (count_exec = 1; count_exec <= TEST_EXEC_TIMES; ++count_exec)
+		{
+			tresult = run_test_serial_rcm(matrices[count_matrix]);
+			fprintf(out_file, "%.6f |", tresult.time);
+		}
+		fprintf(out_file, "\n\n");
+		
+		for (count_percents = 0; count_percents < num_bfs_percents; ++count_percents)
+		{
+			fprintf(out_file, "Dinamyc Chunk: %f\n\n", bfs_chunk_percent[count_percents]);
+			printf("\nDinamyc Chunk: %f\n", bfs_chunk_percent[count_percents]);
+			
+			print_head_table_result(out_file);
+			
+			for (count_nthreads = 0; count_nthreads < num_nthreads; ++count_nthreads)
+			{
+				fprintf(out_file, "|Unordered      |   %d    |", nthreads[count_nthreads]);
+				
+				for (count_exec = 1; count_exec <= TEST_EXEC_TIMES; ++count_exec)
+				{
+					tresult = run_test_unordered_rcm(matrices[count_matrix], 
+						nthreads[count_nthreads], bfs_chunk_percent[count_percents]);
+					fprintf(out_file, "%.6f |", tresult.time);
+				}
+				fprintf(out_file, "\n\n");
+			}
+		}
+	}
+	
+	fclose(out_file);
+}
--- UnitTests/rcm_parallel_test.h
+++ UnitTests/rcm_parallel_test.h
@@ -1,5 +1,16 @@
 #include "../CommonFiles/protos_parallel.h"
 #include <assert.h>
 
+#define TEST_EXEC_TIMES 5
+typedef enum EXECUTION { ALL_TESTS, ONE_INSTANCE} EXECUTION;
+typedef struct {
+	double time;
+	long int bandwidth;
+} test_result;
+
 extern void test_prefix_sum();
 extern void test_prefix_sum_parallel_serial(int* counts, int max_level);
+test_result run_test_serial_rcm(const char* path_matrix_file);
+test_result run_test_unordered_rcm(const char* path_matrix_file, const int num_threads, const float bfs_chunk_size);
+extern void run_test_serial_parallel_rcm(const char* path_matrix_file, const int num_threads, const float bfs_chunk_size);
+extern void run_all_tests();
