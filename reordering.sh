ba3aae48d47804ef85311e61d03069d631574c94
tnas <nascimenthiago@gmail.com>
2016-07-27 16:18:06 -0300

    little fixings.

--- reordering.sh
+++ reordering.sh
@@ -7,6 +7,6 @@ export OMP_NUM_THREADS=8
 ./reordering-library -t 1
 
 # Script for executing the reordering-library program into 
-# the cluster Altix-XE from LNCC.
+# the Altix-XE cluster from LNCC.
 # Commando for submit the program to the cluster's queue: 
 # qsub reordering.sh
